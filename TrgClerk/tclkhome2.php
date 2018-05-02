a4ecb4a6c987263a58ae618a2d48348fe12b6c90
akibulalam <38162025+akibulalam@users.noreply.github.com>
2018-05-02 14:34:31 +0600

    Add files via upload

---
+++ TrgClerk/tclkhome2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AUTM - Automated Unit Training Manager</title>
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap-grid.css" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap-reboot.css" type="text/css" />
+	<link rel="stylesheet" href="../css/main.css" type="text/css" />
+
+	
+<style>
+body  {
+    background-image: url("../images/homeback3.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+body, html {
+    height: 100%;
+}
+
+</style>
+        
+
+	
+</head>
+
+<body data-spy="scroll" data-target=".navbar-collapse">
+<div class="container">
+<div class="topnav">
+	<div style="overflow:auto;">
+
+	  <a class="active" href="#home">Home</a>
+	  <a href="tclkcontact.php">Contact</a>
+	  <a href="tclkabout.php">About</a>
+	</div>
+</div>
+<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+		        <label for="comment" style="color: white;"><h3><b>Comment</b></h3></label>
+		     </div>
+			<div class="col-lg-4">
+        		<textarea id="comment" name="comment" placeholder ="Type Your Comment here" style="height:140px; width:500px;"></textarea>
+        		<div class="col-lg-8">
+      			<button>Submit</button>
+      		</div>
+      		</div>
+      		
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+		        <label for="circular" style="color: white;"><h3><b>Circular</b></h3></label>
+		     </div>
+			<div class="col-lg-4">
+        		<textarea id="circular" name="circular" style="height:140px; width:500px;" disabled></textarea>
+        		<div class="col-lg-8" >
+      			<button>Seen</button>
+      		</div>
+      		</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+		        <label for="comment" style="color: white;"><h3><b>Running Circular</b></h3></label>
+		     </div>
+			<div class="col-lg-4">
+        		<textarea id="circular" name="circular"  style="height:120px; width:500px;"></textarea>
+        		<div class="col-lg-8">
+      			<button>Seen</button>
+      		</div>
+      		</div>
+      		
+		</div>
+	</div>
+
+</div>
+
+
+</div>
+</body>
+</html>
+
